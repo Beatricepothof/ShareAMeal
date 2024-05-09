98dcd436992372836adbb5019e5adfe66da94845
Beatricepothof <beatricepothof@gmail.com>
2024-05-09 17:32:56 +0200

    user route create validation fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,61 +16,7 @@ const notFound = (req, res, next) => {
     })
 }
 
-// Input validation functions for user routes
 const validateUserCreate = (req, res, next) => {
-    try {
-        assert(req.body.firstName, 'Missing or incorrect firstName field')
-        chai.expect(req.body.firstName).to.not.be.empty
-        chai.expect(req.body.firstName).to.be.a('string')
-        chai.expect(req.body.firstName).to.match(
-            /^[a-zA-Z]+$/,
-            'firstName must be a string'
-        )
-        logger.trace('User successfully validated')
-        next()
-    } catch (ex) {
-        logger.trace('User validation failed:', ex.message)
-        next({
-            status: 400,
-            message: ex.message,
-            data: {}
-        })
-    }
-}
-
-// Input validation function 2 met gebruik van assert
-const validateUserCreateAssert = (req, res, next) => {
-    try {
-        assert(req.body.emailAdress, 'Missing email')
-        assert(req.body.firstName, 'Missing or incorrect first name')
-        assert(req.body.lastName, 'Missing last name')
-        next()
-    } catch (ex) {
-        next({
-            status: 400,
-            message: ex.message,
-            data: {}
-        })
-    }
-}
-
-// Input validation function 2 met gebruik van assert
-const validateUserCreateChaiShould = (req, res, next) => {
-    try {
-        req.body.firstName.should.not.be.empty.and.a('string')
-        req.body.lastName.should.not.be.empty.and.a('string')
-        req.body.emailAdress.should.not.be.empty.and.a('string').and.match(/@/)
-        next()
-    } catch (ex) {
-        next({
-            status: 400,
-            message: ex.message,
-            data: {}
-        })
-    }
-}
-
-const validateUserCreateChaiExpect = (req, res, next) => {
     try {
         assert(req.body.firstName, 'Missing or incorrect firstName field')
         chai.expect(req.body.firstName).to.not.be.empty

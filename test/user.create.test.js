642e9ebc42b732e1f4fd50402073da6d9b530b01
Beatricepothof <beatricepothof@gmail.com>
2024-05-09 19:45:31 +0200

    validation fixing

--- test/user.create.test.js
+++ test/user.create.test.js
@@ -9,14 +9,19 @@ tracer.setLevel('warn')
 
 const endpointToTest = '/api/user'
 
-describe('UC-201 Register as new user', () => {
-    //
+describe('UC201 Registreren als nieuwe user', () => {
+    /**
+     * Voorbeeld van een beforeEach functie.
+     * Hiermee kun je code hergebruiken of initialiseren.
+     */
     beforeEach((done) => {
         console.log('Before each test')
         done()
     })
 
-    // TC-201-1: Missing required field
+    /**
+     * Hier starten de testcases
+     */
     it('TC-201-1 Verplicht veld ontbreekt', (done) => {
         chai.request(server)
             .post(endpointToTest)
@@ -43,100 +48,100 @@ describe('UC-201 Register as new user', () => {
 
                 done()
             })
+    })
 
-        // TC-201-2: Invalid email address
-        it('TC-201-2 Invalid email address', (done) => {
-            chai.request(server)
-                .post(endpointToTest)
-                .send({
-                    firstName: 'Voornaam',
-                    lastName: 'Achternaam',
-                    emailAdress: 'invalid.email', // Invalid email address
-                    password: '123456'
-                })
-                .end((err, res) => {
-                    /**
-                     * Using chai.expect for assertion
-                     */
-                    chai.expect(res).to.have.status(400)
-                    chai.expect(res.body).to.be.a('object')
-                    chai.expect(res.body)
-                        .to.have.property('message')
-                        .that.is.a('string')
-                        .contains('Invalid email address')
-                    chai.expect(res.body).not.to.have.property('data')
+    // TC-201-2: Invalid email address
+    it('TC-201-2 Invalid email address', (done) => {
+        chai.request(server)
+            .post(endpointToTest)
+            .send({
+                firstName: 'Voornaam',
+                lastName: 'Achternaam',
+                emailAdress: 'invalid.email', // Invalid email address
+                password: '123456'
+            })
+            .end((err, res) => {
+                /**
+                 * Using chai.expect for assertion
+                 */
+                chai.expect(res).to.have.status(400)
+                chai.expect(res.body).to.be.a('object')
+                chai.expect(res.body)
+                    .to.have.property('message')
+                    .that.is.a('string')
+                    .contains('Invalid email address')
+                chai.expect(res.body).not.to.have.property('data')
 
-                    done()
-                })
-        })
+                done()
+            })
+    })
 
-        // TC-201-3: Invalid password
-        it('TC-201-3 Invalid password', (done) => {
-            chai.request(server)
-                .post(endpointToTest)
-                .send({
-                    firstName: 'Voornaam',
-                    lastName: 'Achternaam',
-                    emailAdress: 'test.mail@server.nl',
-                    password: ''
-                })
-                .end((err, res) => {
-                    res.should.have.status(400)
-                    res.body.should.be.a('object')
-                    res.body.should.have
-                        .property('message')
-                        .that.is.a('string')
-                        .contains('Invalid password')
-                    res.body.should.not.have.property('data')
-                    done()
-                })
-        })
+    // TC-201-3: Invalid password
+    it('TC-201-3 Invalid password', (done) => {
+        chai.request(server)
+            .post(endpointToTest)
+            .send({
+                firstName: 'Voornaam',
+                lastName: 'Achternaam',
+                emailAdress: 'test.mail@server.nl',
+                password: ''
+            })
+            .end((err, res) => {
+                res.should.have.status(400)
+                res.body.should.be.a('object')
+                res.body.should.have
+                    .property('message')
+                    .that.is.a('string')
+                    .contains('Invalid password')
+                res.body.should.not.have.property('data')
+                done()
+            })
+    })
 
-        // TC-201-4: User already exists
-        it('TC-201-4 User already exists', (done) => {
-            chai.request(server)
-                .post(endpointToTest)
-                .send({
-                    firstName: 'Voornaam',
-                    lastName: 'Achternaam',
-                    emailAdress: 'test.mail@server.nl',
-                    password: '123456'
-                })
-                .end((err, res) => {
-                    res.should.have.status(403)
-                    res.body.should.be.a('object')
-                    res.body.should.have
-                        .property('message')
-                        .that.is.a('string')
-                        .contains('User already exists')
-                    res.body.should.not.have.property('data')
-                    done()
-                })
-        })
+    // TC-201-4: User already exists
+    it('TC-201-4 User already exists', (done) => {
+        chai.request(server)
+            .post(endpointToTest)
+            .send({
+                firstName: 'Voornaam',
+                lastName: 'Achternaam',
+                emailAdress: 'test.mail@server.nl',
+                password: '123456'
+            })
+            .end((err, res) => {
+                res.should.have.status(403)
+                res.body.should.be.a('object')
+                res.body.should.have
+                    .property('message')
+                    .that.is.a('string')
+                    .contains('User already exists')
+                res.body.should.not.have.property('data')
+                done()
+            })
+    })
 
-        it('TC-201-5 Gebruiker succesvol geregistreerd', (done) => {
-            chai.request(server)
-                .post(endpointToTest)
-                .send({
-                    firstName: 'Voornaam',
-                    lastName: 'Achternaam',
-                    emailAdress: 'v.a@server.nl'
-                })
-                .end((err, res) => {
-                    res.should.have.status(200)
-                    res.body.should.be.a('object')
+    it('TC-201-5 Gebruiker succesvol geregistreerd', (done) => {
+        chai.request(server)
+            .post(endpointToTest)
+            .send({
+                firstName: 'Voornaam',
+                lastName: 'Achternaam',
+                emailAdress: 'v.a@server.nl'
+            })
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('object')
 
-                    res.body.should.have.property('data').that.is.a('object')
-                    res.body.should.have.property('message').that.is.a('string')
+                res.body.should.have.property('data').that.is.a('object')
+                res.body.should.have.property('message').that.is.a('string')
 
-                    const data = res.body.data
-                    data.should.have.property('firstName').equals('Voornaam')
-                    data.should.have.property('lastName').equals('Achternaam')
-                    data.should.have.property('emailAdress')
-                    data.should.have.property('id').that.is.a('number')
+                const data = res.body.data
+                data.should.have.property('firstName').equals('Voornaam')
+                data.should.have.property('lastName').equals('Achternaam')
+                data.should.have.property('emailAdress')
+                data.should.have.property('id').that.is.a('number')
 
-                    done()
-                })
-        })
+                done()
+            })
     })
 })

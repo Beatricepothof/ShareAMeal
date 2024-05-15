174ed5cc93540bd5a1f86bf97f15a28bffa00522
Beatricepothof <beatricepothof@gmail.com>
2024-05-15 10:05:35 +0200

    usercreate validate fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,72 +16,72 @@ const notFound = (req, res, next) => {
     })
 }
 
-// const validateUserCreate = (req, res, next) => {
-//     try {
-//         // Log the email adress just before validation
-//         console.log('Email Adress:', req.body.emailAddress)
+const validateUserCreate = (req, res, next) => {
+    try {
+        // Log the email adress just before validation
+        console.log('Email Adress:', req.body.emailAddress)
 
-//         // Validate required fields
-//         const requiredFields = [
-//             'firstName',
-//             'lastName',
-//             'emailAdress',
-//             'password',
-//             'phoneNumber',
-//             'street',
-//             'city'
-//         ]
-//         for (const field of requiredFields) {
-//             if (!req.body[field]) {
-//                 throw new Error(`Missing or incorrect ${field} field`)
-//             }
-//         }
+        // Validate required fields
+        const requiredFields = [
+            'firstName',
+            'lastName',
+            'emailAdress',
+            'password',
+            'phoneNumber',
+            'street',
+            'city'
+        ]
+        for (const field of requiredFields) {
+            if (!req.body[field]) {
+                throw new Error(`Missing or incorrect ${field} field`)
+            }
+        }
 
-//         // Validate firstName field
-//         if (!/^[a-zA-Z]+$/.test(req.body.firstName)) {
-//             throw new Error('firstName must be a string')
-//         }
+        // Validate firstName field
+        if (!/^[a-zA-Z]+$/.test(req.body.firstName)) {
+            throw new Error('firstName must be a string')
+        }
 
-//         // Validate email field
-//         if (
-//             !/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3})$/.test(
-//                 req.body.emailAdress
-//             )
-//         ) {
-//             throw new Error('Invalid email adress')
-//         }
+        // Validate email field
+        if (
+            !/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3})$/.test(
+                req.body.emailAdress
+            )
+        ) {
+            throw new Error('Invalid email adress')
+        }
 
-//         // Validate password field
-//         if (!/(?=.*[A-Z])(?=.*\d).{8,}/.test(req.body.password)) {
-//             throw new Error('Invalid password')
-//         }
+        // Validate password field
+        if (!/(?=.*[A-Z])(?=.*\d).{8,}/.test(req.body.password)) {
+            throw new Error('Invalid password')
+        }
 
-//         // Validate phoneNumber field
-//         if (!/^06-\d{8}$/.test(req.body.phoneNumber)) {
-//             throw new Error('Invalid phone number')
-//         }
+        // Validate phoneNumber field
+        if (!/^06-\d{8}$/.test(req.body.phoneNumber)) {
+            throw new Error('Invalid phone number')
+        }
 
-//         // Validate street field
-//         if (typeof req.body.street !== 'string') {
-//             throw new Error('street must be a string')
-//         }
+        // Validate street field
+        if (typeof req.body.street !== 'string') {
+            throw new Error('street must be a string')
+        }
 
-//         // Validate city field
-//         if (typeof req.body.city !== 'string') {
-//             throw new Error('city must be a string')
-//         }
+        // Validate city field
+        if (typeof req.body.city !== 'string') {
+            throw new Error('city must be a string')
+        }
 
-//         logger.trace('User successfully validated for create')
-//         next()
-//     } catch (ex) {
-//         logger.trace('User validation failed for create:', ex.message)
-//         next({
-//             status: 400,
-//             message: ex.message,
-//             data: {}
-//         })
-//     }
-// }
+        logger.trace('User successfully validated for create')
+        next()
+    } catch (ex) {
+        logger.trace('User validation failed for create:', ex.message)
+        next({
+            status: 400,
+            message: ex.message,
+            data: {}
+        })
+    }
+}
 
 const validateUserUpdate = (req, res, next) => {
     try {
